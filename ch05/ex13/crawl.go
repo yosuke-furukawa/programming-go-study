f7c495d4f9e9851db1543245203482d64e884fba
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2018-06-14 04:43:34 +0900

    feat: add ex13

---
+++ ch05/ex13/crawl.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+
+	"os"
+
+	"strings"
+
+	"path"
+
+	"io"
+
+	"golang.org/x/net/html"
+)
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
+
+func extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue // ignore bad URLs
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return links, nil
+}
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func download(u string) error {
+	resp, err := http.Get(u)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	item, _ := url.Parse(u)
+	base := path.Base(item.Path)
+	ext := path.Ext(item.Path)
+	downloaddir := "./download/"
+	downloadpath := downloaddir + item.Path
+
+	if strings.HasSuffix(base, "/") {
+		downloadpath += "index.html"
+	} else if ext == "" {
+		downloadpath += ".html"
+	}
+
+	err = os.MkdirAll(downloaddir+base, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	file, err := os.Create(downloadpath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func crawl(url string) []string {
+	if !strings.HasPrefix(url, hostname) {
+		return nil
+	}
+	fmt.Println(url)
+	download(url)
+	list, err := extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+var hostname string
+
+func main() {
+	hostname = os.Args[1]
+	breadthFirst(crawl, []string{hostname})
+}
