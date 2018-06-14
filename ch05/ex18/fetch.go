9fecc98e216aaf7086abad0737195757f65a4d90
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2018-06-14 12:35:54 +0900

    feat: add ex18

---
+++ ch05/ex18/fetch.go
@@ -0,0 +1,33 @@
+package ex18
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+
+	if local == "/" {
+		local = "index.html"
+	}
+
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	defer func() { err = f.Close() }()
+
+	n, err = io.Copy(f, resp.Body)
+	return local, n, err
+
+}
